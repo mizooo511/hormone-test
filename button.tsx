15134edae766c59c7bf35d0e48eec672185b431f
mizooo511 <mizo2000511@gmail.com>
2025-06-22 02:47:16 +0900

    Add files via upload

---
+++ button.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+export function Button({ children, onClick, className }: { children: React.ReactNode; onClick?: () => void; className?: string }) {
+  return (
+    <button onClick={onClick} className={`transition duration-150 focus:outline-none ${className}`}>
+      {children}
+    </button>
+  );
+}
